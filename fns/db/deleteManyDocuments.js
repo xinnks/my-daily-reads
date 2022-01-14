8892dfe718492b28802f2af07305558626f3203a
James Sinkala <xinnks@gmail.com>
2022-01-14 07:26:15 +0300

    improvement: modify the query's implementation

--- fns/db/deleteManyDocuments.js
+++ fns/db/deleteManyDocuments.js
@@ -5,7 +5,7 @@ const {client, dbName} = require('./client');
  * @param { Object } query => The query to match docs to delete
  * @param { String } collection => Collection name
 **/
-const deleteManyDocuments = async (query, collection) => new Promise(async (resolve, reject) => {
+const deleteManyDocuments = async (query = '', collection) => new Promise(async (resolve, reject) => {
   // Use connect method to connect to the server
   try {
     await client.connect();
@@ -13,9 +13,12 @@ const deleteManyDocuments = async (query, collection) => new Promise(async (reso
     const db = client.db(dbName);
     const dbCollection = db.collection(collection) || db.collection('sources');
     
-    query = query || { title: "None" };
-
-    const result = await dbCollection.deleteMany(query);
+    let result;
+    if(query === ''){
+      result = await dbCollection.deleteMany();
+    } else {
+      result = await dbCollection.deleteMany(query);
+    }
 
     resolve(result.deletedCount);
   } catch(e) {
