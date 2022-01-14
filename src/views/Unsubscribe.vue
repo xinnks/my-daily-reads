7f7f71b5da05977c13a98e115d1c1a0d12fcff06
James Sinkala <xinnks@gmail.com>
2022-01-14 07:57:20 +0300

    style: update style

--- src/views/Unsubscribe.vue
+++ src/views/Unsubscribe.vue
@@ -13,10 +13,12 @@
       {{sentUnsubscriptionRequest ? "We just sent a one time password to your email." : ""}}
     </p>
 
-    <svg :class="`${requestLoading ? 'animate-spin':''} -ml-1 mr-3 h-5 w-5 text-white`" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" v-if="requestLoading">
-      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-    </svg>
+    <div class="flex flex-row justify-center">
+      <svg :class="`${requestLoading ? 'animate-spin':''} -ml-1 mr-3 h-5 w-5 text-gray-700`" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" v-if="requestLoading">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+    </div>
     
     <form-message :formNotification="formNotification" v-if="formNotification.message"></form-message>
 
@@ -56,8 +58,9 @@ notification = ref({type: "", message: ""}),
 formNotification = ref({type: "", message: ""});
 
 email.value = route.params.email;
+// console.log("route.params.email: -- ", route.params.email)
 
-(function unsubscribeRequest() {
+function unsubscribeRequest() {
   if(!email.value){
     formNotification.value.type = "error";
     formNotification.value.message = "email is required!";
@@ -79,7 +82,8 @@ email.value = route.params.email;
     requestLoading.value = false;
     console.log(err);
   })
-})();
+}
+unsubscribeRequest();
 
 function unsubscribe() {
   confirmLoading.value = true;
