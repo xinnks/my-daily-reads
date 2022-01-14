b3c4f933130929c952019cdcc99733510d3f67bc
James Sinkala <xinnks@gmail.com>
2022-01-14 11:43:25 +0300

    improvement: Update button styles

--- src/views/Unsubscribe.vue
+++ src/views/Unsubscribe.vue
@@ -27,12 +27,11 @@
       <label for="otp" class="col-start-1">OTP: </label>
       <input type="otp" v-model="otp" class="col-start-2 col-span-3 h-8 p-4 rounded-sm border-sm ring-2 ring-gray-500" required>
 
-      <button @click.prevent="unsubscribe()" class="col-start-2 col-span-2 h-10 p-2 rounded-md border-gray-700 bg-gray-700 hover:bg-gray-800 text-white ring-2 ring-gray-500 mt-4">
-        <svg :class="`${confirmLoading ? 'animate-spin':''} -ml-1 mr-3 h-5 w-5 text-white`" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-      </svg>
-        {{ confirmLoading ? 'Processing..' : 'UNSUBSCRIBE' }}
+      <button @click.prevent="unsubscribe()" :class="`inline-flex flex-row justify-center col-start-2 col-span-2 h-10 p-2 rounded-md border-gray-700 bg-gray-700 hover:bg-gray-800 text-white ring-2 ring-gray-500 mt-4 ${confirmLoading?'cursor-not-allowed':''}`">
+        <svg v-if="confirmLoading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg> {{ confirmLoading ? 'Processing..' : 'UNSUBSCRIBE' }}
       </button>
     
     </form>
